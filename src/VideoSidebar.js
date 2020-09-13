846164954f946477fd996ef2313541ddf1cbfb8b
alexlecco <alexlecco24@gmail.com>
2020-09-13 07:21:34 -0300

    simulate like action

--- src/VideoSidebar.js
+++ src/VideoSidebar.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./VideoSidebar.css";
 import FavoriteIcon from "@material-ui/icons/Favorite";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import MessageIcon from "@material-ui/icons/Message";
 import ShareIcon from "@material-ui/icons/Share";
 
 function VideoSidebar() {
+  const [liked, setLiked] = useState(false);
+
   return (
     <div className="videoSidebar">
       <div className="videoSidebar__button">
-        <FavoriteIcon fontSize="large" />
-        <p>100</p>
+        {liked ? (
+          <FavoriteIcon fontSize="large" onClick={(e) => setLiked(false)} />
+        ) : (
+          <FavoriteBorderIcon
+            fontSize="large"
+            onClick={(e) => setLiked(true)}
+          />
+        )}
+
+        <p>{liked ? 101 : 100}</p>
       </div>
       <div className="videoSidebar__button">
         <MessageIcon fontSize="large" />
