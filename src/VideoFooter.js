b47441e1b6931e8259eb25fdc753daa20cd489f7
alexlecco <alexlecco24@gmail.com>
2020-09-13 05:36:26 -0300

    create and render VideoFooter

---
+++ src/VideoFooter.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./VideoFooter.css";
+
+function VideoFooter() {
+  return (
+    <div className="videoFooter">
+      <div className="videoFoote__text">
+        <h3>@alexlecco</h3>
+        <p>This is some description</p>
+      </div>
+    </div>
+  );
+}
+
+export default VideoFooter;
