89fe60cb09a05003fc81bc08cbaaa70f4b0c88dd
alexlecco <alexlecco24@gmail.com>
2020-09-13 06:39:38 -0300

    add Ticker component to VideoFooter

--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./VideoFooter.css";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+import Ticker from "react-ticker";
 
 function VideoFooter() {
   return (
@@ -7,6 +9,17 @@ function VideoFooter() {
       <div className="videoFoote__text">
         <h3>@alexlecco</h3>
         <p>This is some description</p>
+        <div className="videoFooter__ticker">
+          <MusicNoteIcon className="videoFooter__icon" />
+
+          <Ticker mode="smooth">
+            {({ index }) => (
+              <>
+                <p>Hold on - Lu2vyk ft Madison Gold</p>
+              </>
+            )}
+          </Ticker>
+        </div>
       </div>
     </div>
   );
